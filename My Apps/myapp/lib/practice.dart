192edd1c4ee272fcf07eb7c182b5db1c5e25bf98
itsabhinavism <a.for.abhinav.2003@gmail.com>
2024-05-16 19:53:18 +0530

    Update practice.dart

--- My Apps/myapp/lib/practice.dart
+++ My Apps/myapp/lib/practice.dart
@@ -1,11 +1,8 @@
 import 'dart:io';
 
-void main () {
-  print ('welcome to dart!');
-  stdout.write ('Enter the name : ');
-  //stdin
-var name = stdin.readLineSync();
-print('Welcome $name');
-
-
-} 
+void main() {
+  print('welcome to dart!');
+  stdout.write('Enter the name : ');
+  var name = stdin.readLineSync();
+  print('Welcome $name');
+}
