6a33d6c772757475f033fec3016c2604929169c6
itsabhinavism <a.for.abhinav.2003@gmail.com>
2024-05-19 19:48:06 +0530

    save

--- My Apps/myapp/lib/practice.dart
+++ My Apps/myapp/lib/practice.dart
@@ -14,10 +14,18 @@ void main() {
   var myC = myClass();
   myC.printName("Abhinav Anand");
   myC.printName("Flutter Developer");
+
+  print(myC.Add(5, 6));
+  print(myC.Add(100, 400));
 }
 
 class myClass {
   void printName(String name) {
     print(name);
+  }
+
+  int Add(int n1, int n2) {
+    int sum = n1 + n2;
+    return sum;
   }
 }
