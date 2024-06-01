dbfda02174533d39d36da6e1cc7ef3be779c5793
itsabhinavism <a.for.abhinav.2003@gmail.com>
2024-06-01 23:15:15 +0530

    Update image.dart

---
+++ My Apps/myapp/lib/image.dart
@@ -0,0 +1,52 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Flutter App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const MyHomePage(title: 'Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page', style: TextStyle(color: Colors.white)),
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      ),
+
+        // Image.asset('assets/images/Flutter.png'),
+      body: Center(child: Container(
+        width: 150,
+        height: 150,
+        child: Image.asset('assets/images/Flutter.png'),),),
+    
+      
+    );
+  }
+}
