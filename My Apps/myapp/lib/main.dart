22b06defd9c8c4f8cee95f080e1addfd611f4f2e
itsabhinavism <a.for.abhinav.2003@gmail.com>
2024-06-02 17:53:52 +0530

    Update main.dart

--- My Apps/myapp/lib/main.dart
+++ My Apps/myapp/lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,13 +17,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: const MyHomePage(title: 'Home Page'),
+      home: const MyHomePage(title: 'Home'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -36,65 +36,120 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page', style: TextStyle(color: Colors.white)),
+        title: Text(widget.title, style: TextStyle(color: Colors.white)),
         backgroundColor: Color.fromARGB(255, 0, 0, 0),
       ),
-      body: Align(
-        alignment: Alignment.bottomCenter,
-        child: Row(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Container(
-                height: 50,
-                color: const Color.fromARGB(255, 0, 0, 0),
-                child: Image.asset(
-                  'assets/images/menu.png',
-                  width: 5,
-                  height: 5,
-                ),
-              ),
+      body: Stack(
+        children: [
+          Center(
+            child: Container(
+              width: 300,
+              height: 100,
+              child: Image.asset('assets/images/Flutter.png'),
             ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                height: 50,
-                color: const Color.fromARGB(255, 0, 0, 0),
-                child: Image.asset(
-                  'assets/images/search.png',
-                  width: 5,
-                  height: 5,
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                height: 50,
-                color: const Color.fromARGB(255, 0, 0, 0),
-                child: Container(
-                  width: 1,
-                  height: 1,
-                  child: Image.asset(
-                    'assets/images/add.png',
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: InkWell(
+                    onTap: () {
+                      print("Tapped on Menu.");
+                    },
+                    onLongPress: () {
+                      print("Long tapped on Menu.");
+                    },
+                    onDoubleTap: () {
+                      print("Double tapped on Menu.");
+                    },
+                    child: Container(
+                      height: 50,
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      child: Image.asset(
+                        'assets/images/menu.png',
+                        width: 5,
+                        height: 5,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                height: 50,
-                color: Color.fromARGB(255, 0, 0, 0),
-                child: Image.asset(
-                  'assets/images/setting.png',
-                  width: 5,
-                  height: 5,
+                Expanded(
+                  flex: 2,
+                  child: InkWell(
+                    onTap: () {
+                      print("Tapped on Search Bar.");
+                    },
+                    onLongPress: () {
+                      print("Long tapped on Search Bar.");
+                    },
+                    onDoubleTap: () {
+                      print("Double tapped on Search Bar.");
+                    },
+                    child: Container(
+                      height: 50,
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      child: Image.asset(
+                        'assets/images/search.png',
+                        width: 5,
+                        height: 5,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 50,
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                    child: InkWell(
+                      onTap: () {
+                        print("Tapped on Add Button.");
+                      },
+                      onLongPress: () {
+                        print("Long tapped on Add Button.");
+                      },
+                      onDoubleTap: () {
+                        print("Double tapped on Add Button.");
+                      },
+                      child: Container(
+                        width: 1,
+                        height: 1,
+                        child: Image.asset(
+                          'assets/images/add.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 50,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    child: InkWell(
+                      onTap: () {
+                        print("Tapped on Settings.");
+                      },
+                      onLongPress: () {
+                        print("Long tapped on Settings.");
+                      },
+                      onDoubleTap: () {
+                        print("Double tapped on Settings.");
+                      },
+                      child: Image.asset(
+                        'assets/images/setting.png',
+                        width: 5,
+                        height: 5,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
