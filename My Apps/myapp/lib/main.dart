03b5145286eb862d28ea857e4c44491a624808dd
itsabhinavism <a.for.abhinav.2003@gmail.com>
2024-06-08 22:28:08 +0530

    Update main.dart

--- My Apps/myapp/lib/main.dart
+++ My Apps/myapp/lib/main.dart
@@ -1,45 +1,3 @@
-import 'dart:collection';
-
-import 'package:flutter/material.dart';
-import 'package:myapp/ui_helper/util.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Flutter App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          textTheme: TextTheme(
-            headlineSmall:
-                TextStyle(fontSize: 101, fontWeight: FontWeight.bold),
-            headlineMedium: TextStyle(
-                color: Colors.red,
-                fontSize: 51,
-                fontWeight: FontWeight.w500,
-                fontStyle: FontStyle.italic),
-          )),
-      home: const MyHomePage(title: 'Home'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
@@ -51,20 +9,34 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: Center(
           child: Container(
-              width: 100,
-              height: 100,
+              width: 400,
+              height: 200,
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                        "Current Time: ${time.hour}:${time.minute}:${time.second} ",
-                        style:
-                            TextStyle(fontSize: 12, color: Colors.lightGreen)),
+                    Row(
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            text: 'Current Time --> ',
+                            style: TextStyle(
+                                fontSize: 25, color: Colors.lightGreen),
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text:
+                                      '${time.hour}hr ${time.minute}min ${time.second}s',
+                                  style: TextStyle(color: Colors.black)),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(height: 10),
                     ElevatedButton(
                         onPressed: () {
                           setState(() {});
                         },
-                        child: Text("Current Time"))
+                        child: Text("Refresh"))
                   ])),
         ));
   }
