960ed19e4fc8acc158c3275633381eef6c104e9d
itsabhinavism <a.for.abhinav.2003@gmail.com>
2024-06-08 18:10:05 +0530

    save

--- My Apps/myapp/lib/main.dart
+++ My Apps/myapp/lib/main.dart
@@ -1,13 +1,14 @@
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
+import 'package:myapp/ui_helper/util.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +16,22 @@ class MyApp extends StatelessWidget {
       title: 'My Flutter App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
-      ),
+          primarySwatch: Colors.blue,
+          textTheme: TextTheme(
+            headlineSmall:
+                TextStyle(fontSize: 101, fontWeight: FontWeight.bold),
+            headlineMedium: TextStyle(color:Colors.red,
+                fontSize: 51,
+                fontWeight: FontWeight.w500,
+                fontStyle: FontStyle.italic),
+          )),
       home: const MyHomePage(title: 'Home'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -34,162 +42,22 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    var phno = TextEditingController();
-    var pw = TextEditingController();
-
-    return  Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Container(
-            width: 350,
-            height: 360,
-        
-            child: Padding(
-              padding: EdgeInsets.only(top:15,bottom:15),
-              child: Card(
-                shadowColor: Colors.lightGreen,
-                elevation: 10,
-                child: Container(
-                  width: 10,
-                  margin: EdgeInsets.all(15),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                          height: 50,
-                          width: 300,
-                          child:
-                            Padding (padding: EdgeInsets.only(right: 150),
-                            child: Text(
-                              "Login",
-                              style: TextStyle(
-                                  fontSize: 45,
-                                  fontFamily:'cursive1',
-                                  color: Colors.lightGreen),
-                            ),),
-                          ),
-
-                          Container(height:12),
-                      TextField(
-                        keyboardType: TextInputType.phone,
-                        controller: phno,
-                        decoration: InputDecoration(
-                          hintText: 'Enter Phone Number',
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(11),
-                              borderSide: BorderSide(
-                                  color: Colors.lightGreen, width: 2)),
-
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(11),
-                              borderSide: BorderSide(
-                                  color: Color.fromARGB(255, 103, 103, 103),
-                                  width: 2)),
-
-                          //suffixText: "Phone number",
-
-                          suffixIcon: Container(
-                            height: 40,
-                            width: 50,
-                            child: Padding(
-                              padding: EdgeInsets.only(left: 15, right: 15),
-                              child: Image.asset(
-                                  'assets/images/icons/eye-regular.png',
-                                  color: Color.fromARGB(255, 103, 103, 103)),
-                            ),
-                          ),
-
-                          prefixIcon: Container(
-                            width: 50,
-                            child: Padding(
-                              padding: EdgeInsets.only(left: 15, right: 15),
-                              child: Image.asset(
-                                  'assets/images/icons/phone-solid.png',
-                                  color: Color.fromARGB(255, 103, 103, 103)),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(height: 15),
-                      TextField(
-                        controller: pw,
-                        obscureText: true,
-                        obscuringCharacter: '*',
-                        decoration: InputDecoration(
-                          hintText: "Enter Password",
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(11),
-                              borderSide: BorderSide(
-                                  color: Colors.lightGreen, width: 2)),
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(11),
-                              borderSide: BorderSide(
-                                  color: Color.fromARGB(255, 103, 103, 103),
-                                  width: 2)),
-                          disabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(11),
-                              borderSide: BorderSide(
-                                  color: Color.fromARGB(255, 103, 103, 103),
-                                  width: 2)),
-                          prefixIcon: Container(
-                            width: 50,
-                            child: Padding(
-                              padding: EdgeInsets.only(left: 15, right: 15),
-                              child: Image.asset(
-                                  'assets/images/icons/key-solid.png',
-                                  color: Color.fromARGB(255, 103, 103, 103)),
-                            ),
-                          ),
-                          suffixIcon: Container(
-                            width: 44,
-                            child: Padding(
-                              padding: EdgeInsets.only(left: 15, right: 15),
-                              child: Image.asset(
-                                  'assets/images/icons/lock-solid.png',
-                                  color: Color.fromARGB(255, 103, 103, 103)),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(height: 13),
-                      Container(
-                        width: 1000,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            String uPhNo = phno.text.toString();
-                            String uPw = pw.text;
-
-                            print("Phone Number: $uPhNo, Password: $uPw");
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      MyHomePage(title: 'Home')),
-                            );
-                          },
-                          child: Text("Submit",style: TextStyle(
-        color: const Color.fromARGB(255, 255, 255, 255),
-      )),
-  style: ButtonStyle(
-    backgroundColor: MaterialStateProperty.all<Color>(Colors.lightGreen),
-  
-                        ),
-                        ),
-                      ),
-                      Container(height:9),
-                      Center(child: Column(children:[
-                        Text('Forgotten your password? Eat almonds daily...',
-                            style: TextStyle(
-                                fontSize: 12,
-                                fontStyle: FontStyle.italic,
-                                color: Colors.grey)),],),
-                      ),Container(height:9),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ));
+    var time= DateTime.now();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Clock Widget'),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Center(child: Container(width:100,height:100,
+      child: Column(mainAxisAlignment:MainAxisAlignment.center,
+      
+      children:[Text("Current Time: ${time.hour}:${time.minute}:${time.second} ",
+      style:TextStyle(fontSize:12,color:Colors.lightGreen)),
+      ElevatedButton(onPressed: (), )
+      
+      ])),
+      
+      )
+    );
   }
 }
