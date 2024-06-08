ad9db94daddb836a6e314aec3008000320021481
itsabhinavism <a.for.abhinav.2003@gmail.com>
2024-06-08 22:46:57 +0530

    Update main.dart

--- My Apps/myapp/lib/main.dart
+++ My Apps/myapp/lib/main.dart
@@ -1,13 +1,44 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Flutter App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: const MyHomePage(title: 'Home'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    var time = DateTime.now();
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Clock Widget'),
-          backgroundColor: Colors.lightGreen,
-        ),
-        body: Center(
+var time= DateTime.now();
+
+    return  Scaffold(
+        appBar: AppBar(),
+        body: Center(child:Card(shadow
           child: Container(
               width: 400,
               height: 200,
@@ -15,7 +46,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Row(
-                      children: [
+                      children: [shadowColor: Colors.lightGreen,
+                elevation: 10,
                         RichText(
                           text: TextSpan(
                             text: 'Current Time --> ',
