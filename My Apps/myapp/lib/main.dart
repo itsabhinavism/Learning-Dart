6b82d1421e3aa13f2ea283c0f9478092f10fbb84
itsabhinavism <a.for.abhinav.2003@gmail.com>
2024-06-08 11:16:44 +0530

    save

--- My Apps/myapp/lib/main.dart
+++ My Apps/myapp/lib/main.dart
@@ -38,14 +38,11 @@ class _MyHomePageState extends State<MyHomePage> {
     var pw = TextEditingController();
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Login Page', style: TextStyle(color: Colors.white)),
-          backgroundColor: Colors.lightGreen,
-        ),
+        appBar: AppBar(),
         body: Center(
           child: Container(
             width: 380,
-            height: 300,
+            height: 320,
             margin: EdgeInsets.all(8),
             child: Padding(
               padding: EdgeInsets.all(10),
@@ -53,125 +50,140 @@ class _MyHomePageState extends State<MyHomePage> {
                 shadowColor: Colors.lightGreen,
                 elevation: 10,
                 child: Container(
-                    width: 10,
-                    margin: EdgeInsets.all(15),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TextField(
-                          keyboardType: TextInputType.phone,
-                          controller: phno,
-                          decoration: InputDecoration(
-                            hintText: 'Enter Phone Number',
-                            focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(11),
-                                borderSide: BorderSide(
-                                    color: Colors.lightGreen, width: 2)),
-
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(11),
-                                borderSide: BorderSide(
-                                    color: Color.fromARGB(255, 103, 103, 103),
-                                    width: 2)),
-
-                            //suffixText: "Phone number",
-
-                            suffixIcon: Container(
-                              height: 40,
-                              width: 50,
-                              child: Padding(
-                                padding: EdgeInsets.only(left: 15, right: 15),
-                                child: Image.asset(
-                                    'assets/images/icons/eye-regular.png',
-                                    color: Color.fromARGB(255, 103, 103, 103)),
-                              ),
-                            ),
-
-                            prefixIcon: Container(
-                              width: 50,
-                              child: Padding(
-                                padding: EdgeInsets.only(left: 15, right: 15),
-                                child: Image.asset(
-                                    'assets/images/icons/phone-solid.png',
-                                    color: Color.fromARGB(255, 103, 103, 103)),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(height: 13),
-                        TextField(
-                          controller: pw,
-                          obscureText: true,
-                          obscuringCharacter: '*',
-                          decoration: InputDecoration(
-                            hintText: "Enter Password",
-                            focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(11),
-                                borderSide: BorderSide(
-                                    color: Colors.lightGreen, width: 2)),
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(11),
-                                borderSide: BorderSide(
-                                    color: Color.fromARGB(255, 103, 103, 103),
-                                    width: 2)),
-                            disabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(11),
-                                borderSide: BorderSide(
-                                    color: Color.fromARGB(255, 103, 103, 103),
-                                    width: 2)),
-                            prefixIcon: Container(
-                              width: 50,
-                              child: Padding(
-                                padding: EdgeInsets.only(left: 15, right: 15),
-                                child: Image.asset(
-                                    'assets/images/icons/key-solid.png',
-                                    color: Color.fromARGB(255, 103, 103, 103)),
-                              ),
-                            ),
-                            suffixIcon: Container(
-                              width: 30,
-                              child: Padding(
-                                padding: EdgeInsets.only(left: 15, right: 15),
-                                child: Image.asset(
-                                    'assets/images/icons/lock-solid.png',
-                                    color: Color.fromARGB(255, 103, 103, 103)),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(height: 20),
-                        Container(
-                          width: 1000,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              String uPhNo = phno.text.toString();
-                              String uPw = pw.text;
-
-                              print("Phone Number: $uPhNo, Password: $uPw");
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        MyHomePage(title: 'Home')),
-                              );
-                            },
-                            child: Text("Login"),
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.lightGreen, // background color
-                              onPrimary: Colors.white, // text color
-                            ),
-                          ),
-                        ),
-                        Container(height: 10),
-                        Center(
-                          child: Text('Forgotten your password?',
+                  width: 10,
+                  margin: EdgeInsets.all(15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                          height: 50,
+                          width: 300,
+                          child:
+                            Padding (padding: EdgeInsets.only(right: 200),
+                            child: Text(
+                              "Login",
                               style: TextStyle(
-                                  fontSize: 12,
-                                  fontStyle: FontStyle.italic,
-                                  color: Colors.grey)),
+                                  fontSize: 30,
+                                  fontFamily:'cursive1',
+                                  color: Colors.lightGreen),
+                            ),),
+                          ),
+
+                          Container(height:10),
+                      TextField(
+                        keyboardType: TextInputType.phone,
+                        controller: phno,
+                        decoration: InputDecoration(
+                          hintText: 'Enter Phone Number',
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(11),
+                              borderSide: BorderSide(
+                                  color: Colors.lightGreen, width: 2)),
+
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(11),
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 103, 103, 103),
+                                  width: 2)),
+
+                          //suffixText: "Phone number",
+
+                          suffixIcon: Container(
+                            height: 40,
+                            width: 50,
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 15, right: 15),
+                              child: Image.asset(
+                                  'assets/images/icons/eye-regular.png',
+                                  color: Color.fromARGB(255, 103, 103, 103)),
+                            ),
+                          ),
+
+                          prefixIcon: Container(
+                            width: 50,
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 15, right: 15),
+                              child: Image.asset(
+                                  'assets/images/icons/phone-solid.png',
+                                  color: Color.fromARGB(255, 103, 103, 103)),
+                            ),
+                          ),
                         ),
-                      ],
-                    )),
+                      ),
+                      Container(height: 13),
+                      TextField(
+                        controller: pw,
+                        obscureText: true,
+                        obscuringCharacter: '*',
+                        decoration: InputDecoration(
+                          hintText: "Enter Password",
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(11),
+                              borderSide: BorderSide(
+                                  color: Colors.lightGreen, width: 2)),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(11),
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 103, 103, 103),
+                                  width: 2)),
+                          disabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(11),
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 103, 103, 103),
+                                  width: 2)),
+                          prefixIcon: Container(
+                            width: 50,
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 15, right: 15),
+                              child: Image.asset(
+                                  'assets/images/icons/key-solid.png',
+                                  color: Color.fromARGB(255, 103, 103, 103)),
+                            ),
+                          ),
+                          suffixIcon: Container(
+                            width: 30,
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 15, right: 15),
+                              child: Image.asset(
+                                  'assets/images/icons/lock-solid.png',
+                                  color: Color.fromARGB(255, 103, 103, 103)),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(height: 20),
+                      Container(
+                        width: 1000,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            String uPhNo = phno.text.toString();
+                            String uPw = pw.text;
+
+                            print("Phone Number: $uPhNo, Password: $uPw");
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      MyHomePage(title: 'Home')),
+                            );
+                          },
+                          child: Text("Login",
+                              style: (TextStyle(
+                                color: Colors.lightGreen,
+                              ))),
+                        ),
+                      ),
+                      Container(height:13),
+                      Center(
+                        child: Text('Forgotten your password?',
+                            style: TextStyle(
+                                fontSize: 12,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.grey)),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
