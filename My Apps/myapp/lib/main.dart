074f2c5e2906424fb21b0199481ffa2c4bc0a388
itsabhinavism <a.for.abhinav.2003@gmail.com>
2024-06-11 07:03:24 +0530

    Update main.dart

--- My Apps/myapp/lib/main.dart
+++ My Apps/myapp/lib/main.dart
@@ -39,38 +39,41 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Current Time Finder'),
+        title: Text('Date Selector',style: TextStyle(color:Colors.white,fontFamily: 'cursive1',fontSize:30)),
         backgroundColor: Colors.lightGreen,
       ),
       body: Center(
         child: Card(
           shadowColor: Colors.lightGreen,
-          elevation: 30,
+          elevation: 20,
           child: Container(
-            width: 300,
-            height: 280,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Selected Date',
-                  style: TextStyle(fontSize: 20, color: Colors.lightGreen),
-                ),
-                ElevatedButton(
-                    onPressed: () async {
-                      DateTime? datePicked = await showDatePicker(
-                          context: context,
-                          initialDate: DateTime.now(),
-                          firstDate: DateTime(2021),
-                          lastDate: DateTime(2025));
+            width: 250,
+            height: 115,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Select date',
+                    style: TextStyle(fontSize: 30, color: Colors.lightGreen, fontFamily: 'cursive1' ),
+                  ),
+                  Container(height: 5),
+                  ElevatedButton(
+                      onPressed: () async {
+                        DateTime? datePicked = await showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime(2021),
+                            lastDate: DateTime(2025));
 
-                      if (datePicked != null) {
-                        print(
-                            'Date selected: ${datePicked.day}--${datePicked.month}--${datePicked.year}');
-                      }
-                    },
-                    child: Text('Show')),
-              ],
+                        if (datePicked != null) {
+                          print(
+                              'Date selected: ${datePicked.day}--${datePicked.month}--${datePicked.year}');
+                        }
+                      },
+                      child: Text('Calendar')),
+                ],
+              ),
             ),
           ),
         ),
