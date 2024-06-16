5553a8a767327506935c1655f799ca8e8e064cdb
itsabhinavism <a.for.abhinav.2003@gmail.com>
2024-06-16 22:54:05 +0530

    Update main.dart

--- My Apps/myapp/lib/main.dart
+++ My Apps/myapp/lib/main.dart
@@ -14,9 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Flutter App',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-   
-      ),
+      theme: ThemeData(),
       home: const MyHomePage(title: 'Home'),
     );
   }
@@ -35,40 +33,74 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     var time = DateTime.now();
-    var arrColors= [Colors.black,
-    Color.fromARGB(255, 0, 234, 255),
-    Colors.green,
-    Color.fromARGB(255, 0, 255, 30),
-    Colors.yellow,Colors.pink,
-    Colors.blueAccent,
-    Colors.purple,
-    Colors.red,
-    Colors.orange];
+    var arrColors = [
+      Colors.black,
+      Color.fromARGB(255, 0, 234, 255),
+      Colors.green,
+      Color.fromARGB(255, 0, 255, 30),
+      Colors.yellow,
+      Colors.pink,
+      Colors.blueAccent,
+      Colors.purple,
+      Colors.red,
+      Colors.orange
+    ];
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Scheduler',
-              style: TextStyle(
-                  color: Colors.white, fontFamily: 'font1', fontSize: 30)),
-          backgroundColor: Colors.lightGreen,
+      appBar: AppBar(
+        title: Text('Grid View',
+            style: TextStyle(
+                color: Colors.white, fontFamily: 'font1', fontSize: 30)),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Center(
+        child: Container(
+          width: 1200,
+          child: GridView.count(
+            crossAxisCount: 4,
+            crossAxisSpacing: 21,
+            mainAxisSpacing: 21,
+            children: [
+              Container(
+                color: arrColors[1],
+              ),
+              Container(
+                color: arrColors[2],
+              ),
+              Container(
+                color: arrColors[3],
+              ),
+              Container(
+                color: arrColors[4],
+              ),
+              Container(
+                color: arrColors[5],
+              ),
+              Container(
+                color: arrColors[6],
+              ),
+              Container(
+                color: arrColors[7],
+              ),
+              Container(
+                color: arrColors[8],
+              ),
+              Container(
+                color: arrColors[4],
+              ),
+              Container(
+                color: arrColors[3],
+              ),
+              Container(
+                color: arrColors[2],
+              ),
+              Container(
+                color: arrColors[1],
+              ),
+            ],
+          ),
         ),
-        body:  Container(
-          width:200,
-          child: GridView.count(crossAxisCount : 5,
-          crossAxisSpacing:11, mainAxisSpacing:11,
-           children:[
- Container(color:arrColors[1],),
-  Container(color:arrColors[2],),
-   Container(color:arrColors[3],),
-    Container(color:arrColors[4],),
-     Container(color:arrColors[5],),
-      Container(color:arrColors[6],),
-       Container(color:arrColors[7],),
-        Container(color:arrColors[8],),
-
-          ],
-        )
-        )
-        );
+      ),
+    );
   }
 }
