c789d4087d891ec30e77c7ca4cf5a7d53f3e53aa
itsabhinavism <a.for.abhinav.2003@gmail.com>
2025-01-26 05:56:02 +0530

    save

--- My Apps/myapp/lib/main.dart
+++ My Apps/myapp/lib/main.dart
@@ -34,59 +34,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page', style: TextStyle(color: Colors.white)),
-        backgroundColor: Color.fromARGB(255, 255, 0, 0),
-      ),
-      body: Center(
-        child: RoundedButton(
-          btnName: 'Login',
-          icon: Icon(Icons.lock),
-          callBack: () {
-            print('Logged in!');
-          },
-          textStyle: TextStyle(fontFamily: 'Kanit-Bold'),
+        appBar: AppBar(
+          title: Text('Home Page', style: TextStyle(color: Colors.white)),
+          backgroundColor: Color.fromARGB(255, 255, 0, 0),
         ),
-      ),
-    );
-  }
-}
-
-class RoundedButton extends StatelessWidget {
-  final String btnName;
-  final Icon icon;
-  final VoidCallback callBack;
-  final TextStyle textStyle;
-
-  RoundedButton({
-    required this.btnName,
-    required this.icon,
-    required this.callBack,
-    required this.textStyle,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: callBack,
-      style: ElevatedButton.styleFrom(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12), // Rounded corners
-        ),
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        backgroundColor: Colors.blue, // Background color
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          icon,
-          SizedBox(width: 8), // Spacing between icon and text
-          Text(
-            btnName,
-            style: textStyle.copyWith(color: Colors.white),
-          ),
-        ],
-      ),
-    );
+        body: RoundedButton(
+          
+        ));
   }
 }
