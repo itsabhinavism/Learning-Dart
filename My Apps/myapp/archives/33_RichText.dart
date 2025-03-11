7dd1ef34dbd15abd85fd8a758003b4af5d84951c
itsabhinavism <a.for.abhinav.2003@gmail.com>
2025-03-11 19:02:41 +0530

    Update 33_RichText.dart

---
+++ My Apps/myapp/archives/33_RichText.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Flutter App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page', style: TextStyle(color: Colors.white)),
+          backgroundColor: const Color.fromARGB(255, 255, 0, 0),
+        ),
+        body: RichText/*default textstyle*/(
+            text: TextSpan(
+                style: TextStyle(color: Colors.red, fontSize: 11),
+                children: <TextSpan>[
+              TextSpan(text: 'Hello'),
+              TextSpan(
+                  text: 'World',
+                  style: TextStyle(
+                      fontSize: 34,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold)),
+              TextSpan(
+                  text: 'Welcome to Flutter',
+                  style: TextStyle(
+                      fontSize: 45,
+                      color: const Color.fromARGB(255, 22, 198, 75),
+                      fontWeight: FontWeight.bold))
+            ])));
+  }
+}
