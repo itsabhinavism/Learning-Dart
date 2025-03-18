65c5d84d7d71e121a4541781101165b66699f28b
itsabhinavism <a.for.abhinav.2003@gmail.com>
2025-03-19 00:55:53 +0530

    Update 34_positioned_widget.dart

---
+++ My Apps/myapp/archives/34_positioned_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Flutter App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        /**appBar: AppBar(
+          title: const Text('Home Page', style: TextStyle(color: Colors.white)),
+          backgroundColor: const Color.fromARGB(255, 255, 0, 0),
+        ),**/
+        body: Container(
+            width: 300,
+            height: double.infinity, color:Colors.grey,
+            child: Stack(children: [
+              Positioned(
+                  bottom: 41,
+                  right: 41,
+                  child:
+                      Container(width: 100, height: 100, color: Colors.white))
+            ])));
+  }
+}
