904d10f06f8dc878f0641bb43b128ec0f8455822
itsabhinavism <a.for.abhinav.2003@gmail.com>
2025-03-26 01:49:29 +0530

    save

--- My Apps/myapp/lib/main.dart
+++ My Apps/myapp/lib/main.dart
@@ -43,82 +43,15 @@ class _MyHomePageState extends State<MyHomePage> {
           title: const Text('Home Page'),
           backgroundColor: const Color.fromARGB(255, 191, 154, 255),
         ),
-        body: Container(
-            color: const Color.fromARGB(255, 121, 184, 236),
-            child: Center(
-                child: Padding(
-                    padding: const EdgeInsets.all(200.0),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          TextField(
-                              keyboardType: TextInputType.number,
-                              controller: no1Controller),
-                          TextField(
-                              keyboardType: TextInputType.number,
-                              controller: no2Controller),
-                          Padding(
-                              padding: const EdgeInsets.all(20.0),
-                              child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    ElevatedButton(
-                                        onPressed: () {
-                                          var no1 = int.parse(
-                                              no1Controller.text.toString());
-                                          var no2 = int.parse(
-                                              no2Controller.text.toString());
-
-                                          var sum = no1 + no2;
-                                          result =
-                                              "The sum of $no1 and $no2 is $sum";
-                                          setState(() {});
-                                        },
-                                        child: Text('Add')),
-                                    ElevatedButton(
-                                        onPressed: () {
-                                          var no1 = int.parse(
-                                              no1Controller.text.toString());
-                                          var no2 = int.parse(
-                                              no2Controller.text.toString());
-                                          var difference = no1 - no2;
-                                          result =
-                                              "The difference of $no1 - $no2 is $difference.";
-                                          setState(() {});
-                                        },
-                                        child: Text('Sub')),
-                                    ElevatedButton(
-                                        onPressed: () {
-                                          var no1 = int.parse(
-                                              no1Controller.text.toString());
-                                          var no2 = int.parse(
-                                              no2Controller.text.toString());
-                                          var product = no1 * no2;
-                                          result =
-                                              "The product of $no1 and $no2 is $product.";
-                                          setState(() {});
-                                        },
-                                        child: Text('Mult')),
-                                    ElevatedButton(
-                                        onPressed: () {
-                                          var no1 = int.parse(
-                                              no1Controller.text.toString());
-                                          var no2 = int.parse(
-                                              no2Controller.text.toString());
-                                          var divisor = no1/no2;
-                                          result =
-                                              "The quotient of $no1 and $no2 is $divisor.";setState((){});
-                                        },
-                                        child: Text('Div')),
-                                  ])),
-                          Padding(
-                            padding: const EdgeInsets.all(11.0),
-                            child: Text(
-                              result,
-                              style: TextStyle(fontSize: 25),
-                            ),
-                          )
-                        ])))));
+        body: ConstrainedBox(
+            constraints: BoxConstraints(
+              
+              maxWidth: 90,
+            
+              maxHeight: 90,
+            ),
+            child:
+            ElevatedButton(onPressed: () {}, child: Text('Clicrld vv vHello WorldHello WorldHello WorldHello WorldHello WorldHello World vv vHello WorldHello WorldHello WorldHello WorldHello WorldHello Wok', overflow: TextOverflow.fade)
+            )));
   }
 }
